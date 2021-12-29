c5187105e9d0de8148a17f715aaa713ef597009a
Safak Yildiz <shafakyildiz@gmail.com>
2021-12-30 00:34:04 +0300

    set start date of end date as starting value

--- src/App.js
+++ src/App.js
@@ -2,31 +2,43 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let checkinDate = document.getElementById("checkin");
-  let checkoutDate = document.getElementById("checkout");
-
-  const [startDate, setStartDate] = useState(
-    document.getElementById("checkin").value
-  );
-
-  const [endDate, setEndDate] = useState(
-    document.getElementById("checkout").value
-  );
-
-  setInterval(() => {
-    console.log(startDate);
-    console.log(endDate);
-  }, 2000);
-
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+  const handleSelectStart = () => {
+    console.log("checkin date is : ", document.getElementById("checkin").value);
+    setStartDate(document.getElementById("checkin").value);
+    setEndDate(document.getElementById("checkin").value);
+    document.getElementById("checkout").value =
+      document.getElementById("checkin").value;
+  };
+  const handleSelectEnd = () => {
+    console.log(
+      "checkout date is : ",
+      document.getElementById("checkout").value
+    );
+    setEndDate(document.getElementById("checkout").value);
+  };
   return (
     <div className="App">
       <div className="container">
         <label htmlFor="checkin">Check-in: </label>
-        <input type="date" id="checkin" name="checkin" />
+        <input
+          type="date"
+          id="checkin"
+          name="checkin"
+          onSelect={handleSelectStart}
+        />
+        <div>{startDate}</div>
       </div>
       <div className="container">
         <label htmlFor="checkout">Check-out: </label>
-        <input type="date" id="checkout" name="checkout" />
+        <input
+          type="date"
+          id="checkout"
+          name="checkout"
+          onSelect={handleSelectEnd}
+        />
+        <div>{endDate}</div>
       </div>
     </div>
   );
