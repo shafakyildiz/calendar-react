468455d8a785c4c0ee0c5d51addebe8f7d9463d8
Safak Yildiz <shafakyildiz@gmail.com>
2021-12-29 23:54:31 +0300

    cleaned app.js and added input as date

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let checkinDate = document.getElementById("checkin");
+  let checkoutDate = document.getElementById("checkout");
+
+  const [startDate, setStartDate] = useState(
+    document.getElementById("checkin").value
+  );
+
+  const [endDate, setEndDate] = useState(
+    document.getElementById("checkout").value
+  );
+
+  setInterval(() => {
+    console.log(startDate);
+    console.log(endDate);
+  }, 2000);
+
   return (
     <div className="App">
-      <h1>custom datepicker</h1>
+      <div className="container">
+        <label htmlFor="checkin">Check-in: </label>
+        <input type="date" id="checkin" name="checkin" />
+      </div>
+      <div className="container">
+        <label htmlFor="checkout">Check-out: </label>
+        <input type="date" id="checkout" name="checkout" />
+      </div>
     </div>
   );
 }
